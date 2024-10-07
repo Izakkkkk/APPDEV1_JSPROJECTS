04d5e802427b093df4b8610f0656139a4f13f173
joemer avendaño <avendao_j@f112.lan>
2024-10-07 15:02:25 +0800

    added my react-cond-rendering

---
+++ jsprojects/MIDTERMS/react-cond-rendering/src/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer(){
+    return(
+        <>
+        <hr />
+        <footer>
+        <p>&copy; {new Date().getFullYear()} My Other Food Website || Written By: Joemer Izak Avendano</p>
+        </footer>
+        </>
+
+    )
+}
---
+++ jsprojects/MIDTERMS/react-cond-rendering/src/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header(){
+    return (
+        
+      <header>
+      <nav>
+      <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contacts</a></li>
+      </ul>
+      </nav>
+      </header>  
+    
+        )
+}
