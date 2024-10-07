4389b265eaf52a44dbb5645431ad98d63f9ae6e8
joemer avendaño <avendao_j@f112.lan>
2024-10-07 15:55:17 +0800

    adding my updating-arrays-react project

---
+++ jsprojects/MIDTERMS/updating-arrays-react/src/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    return (
+        <>
+        <header>
+        <h1>This is my list of Favorite Games:</h1>
+        <hr />
+        </header>
+        </>
+    )
+}
